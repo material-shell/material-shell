dc371fae6109b130d9b5a845813e3a9ae6fc5b49
PapyElGringo <adrienpeslerbe@gmail.com>
2019-07-05 18:31:25 +0200

    fix removing dialogLayout

--- tilingManager/tilingLayouts/layoutByKey.js
+++ tilingManager/tilingLayouts/layoutByKey.js
@@ -3,11 +3,9 @@ const Me = imports.misc.extensionUtils.getCurrentExtension();
 
 const { MaximizeLayout } = Me.imports.tilingManager.tilingLayouts.maximize;
 const { GridLayout } = Me.imports.tilingManager.tilingLayouts.grid;
-const { DialogLayout } = Me.imports.tilingManager.tilingLayouts.dialog;
 
 /* exported TilingLayoutByKey */
 var TilingLayoutByKey = {
     grid: GridLayout,
-    maximize: MaximizeLayout,
-    dialog: DialogLayout
+    maximize: MaximizeLayout
 };
--- tilingManager/tilingManager.js
+++ tilingManager/tilingManager.js
@@ -3,10 +3,6 @@ const Main = imports.ui.main;
 const GLib = imports.gi.GLib;
 const Me = imports.misc.extensionUtils.getCurrentExtension();
 
-const MaximizeLayout =
-    Me.imports.tilingManager.tilingLayouts.maximize.MaximizeLayout;
-const GridLayout = Me.imports.tilingManager.tilingLayouts.grid.GridLayout;
-const DialogLayout = Me.imports.tilingManager.tilingLayouts.dialog.DialogLayout;
 /* exported TilingManager */
 var TilingManager = class TilingManager {
     constructor() {
@@ -14,53 +10,7 @@ var TilingManager = class TilingManager {
         this.grabInProgress = false;
         this.signals = [];
         this.windows = [];
-
-        //this.dialogLayout = new DialogLayout();
     }
-
-    /* onWindowsChanged() {
-        this.windows = this.getFilteredWindows();
-        this.registerWindowsSignal();
-        let [dialogWindows, regularWindows] = this.getDialogAndRegularWindows();
-        this.dialogLayout.onWindowsChanged(dialogWindows);
-        this.layout.onWindowsChanged(regularWindows);
-        this.tileWindows();
-    }
-
-    onWindowFocusedChanged(window, oldWindow) {
-        this.layout.onFocusChanged(window, oldWindow);
-        this.dialogLayout.onFocusChanged(window, oldWindow);
-    }
-
-    registerWindowsSignal() {
-        this.unregisterAllWindowsSignal();
-        this.workspaceEnhancer.windows.forEach(window => {
-            this.signals.push({
-                from: window,
-                id: window.connect('position-changed', window => {
-                    if (!window.grabbed && !this.tilingInProgress) {
-                        this.tileWindows();
-                    }
-                })
-            });
-
-            this.signals.push({
-                from: window,
-                id: window.connect('size-changed', window => {
-                    if (!window.grabbed && !this.tilingInProgress) {
-                        this.tileWindows();
-                    }
-                })
-            });
-        });
-    }
-
-    unregisterAllWindowsSignal() {
-        this.signals.forEach(signal => {
-            signal.from.disconnect(signal.id);
-        });
-        this.signals = [];
-    } */
 
     getFilteredWindows(windows) {
         return windows.filter(window => {
