52eadd27f68a372a944397c2922169f52f0d9f4f
Florian Mounier <paradoxxx.zero@gmail.com>
2019-07-18 16:36:43 +0200

    Remove unecessary metaWindow (as discussed)

--- tilingManager/tilingLayouts/baseTiling.js
+++ tilingManager/tilingLayouts/baseTiling.js
@@ -83,8 +83,8 @@ var BaseTilingLayout = class BaseTilingLayout {
     }
 
     moveMetaWindow(metaWindow, x, y) {
-        this.callSafely(metaWindow, metaWindowInside => {
-            metaWindowInside.move_frame(true, x, y);
+        this.callSafely(metaWindow, () => {
+            metaWindow.move_frame(true, x, y);
         });
     }
 
@@ -112,8 +112,8 @@ var BaseTilingLayout = class BaseTilingLayout {
         ) {
             return;
         }
-        this.callSafely(metaWindow, metaWindowInside => {
-            const actor = metaWindowInside.get_compositor_private();
+        this.callSafely(metaWindow, () => {
+            const actor = metaWindow.get_compositor_private();
             const oldRect = metaWindow.get_frame_rect();
             const [px, py] = global.get_pointer();
 
@@ -156,12 +156,12 @@ var BaseTilingLayout = class BaseTilingLayout {
         if (actor && actor.get_texture()) {
             // We need the actor to be mapped to remove random crashes
             if (actor.mapped) {
-                callback(metaWindow);
+                callback();
             } else {
                 // Wait for it to be mapped
                 if (actor.waitToBeMappedId) return;
                 actor.waitToBeMappedId = actor.connect('notify::mapped', () => {
-                    callback(metaWindow);
+                    callback();
                     actor.disconnect(actor.waitToBeMappedId);
                     delete actor.waitToBeMappedId;
                 });
