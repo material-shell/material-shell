728b35bcf4ab5d3c5411798e851c507b424caf88
Florian Mounier <paradoxxx.zero@gmail.com>
2019-07-24 11:09:34 +0200

    Remove actor waiting for split since it's now useless

--- tilingManager/tilingLayouts/split.js
+++ tilingManager/tilingLayouts/split.js
@@ -157,27 +157,6 @@ var SplitLayout = class SplitLayout extends BaseGrabbableLayout {
         });
     }
 
-    waitForAllActors(actors, callback, retryCount) {
-        // actors.forEach(actor => {
-        //     log(actor, actor.mapped, actor.get_texture(), retryCount);
-        // });
-        if (
-            actors.every(actor => actor && actor.mapped && actor.get_texture())
-        ) {
-            callback();
-        } else if (!retryCount || retryCount < 20) {
-            // log('Retrying', retryCount);
-
-            // If we don't have actor we hope to get it in the next loop
-            GLib.timeout_add(GLib.PRIORITY_DEFAULT, 50, () => {
-                this.waitForAllActors(actors, callback, (retryCount || 0) + 1);
-                return GLib.SOURCE_REMOVE;
-            });
-        } else {
-            log('Failed to transition actors');
-        }
-    }
-
     transition(newMetaWindows, oldMetaWindows) {
         log('start transition');
         const { regularWindows } = this.getDialogAndRegularWindows();
@@ -202,56 +181,54 @@ var SplitLayout = class SplitLayout extends BaseGrabbableLayout {
         );
         allWindows.forEach(window => window.show());
 
-        this.waitForAllActors(allWindows, () => {
-            allMetaWindows
-                .map(metaWindow => metaWindow.get_compositor_private())
-                .forEach(window => {
-                    let rect = window.meta_window.get_frame_rect();
-                    let actorContent = Shell.util_get_content_for_window_actor(
-                        window,
-                        rect
-                    );
-                    let actorClone = new St.Widget({
-                        content: actorContent
-                    });
-                    actorClone.set_size(rect.width, rect.height);
-                    this.transitionContainer.add_child(actorClone);
-                    window.hide();
+        allMetaWindows
+            .map(metaWindow => metaWindow.get_compositor_private())
+            .forEach(window => {
+                let rect = window.meta_window.get_frame_rect();
+                let actorContent = Shell.util_get_content_for_window_actor(
+                    window,
+                    rect
+                );
+                let actorClone = new St.Widget({
+                    content: actorContent
                 });
+                actorClone.set_size(rect.width, rect.height);
+                this.transitionContainer.add_child(actorClone);
+                window.hide();
+            });
 
-            // Get the full workArea here and not workspaceBounds which have gaps
-            const workArea = Main.layoutManager.getWorkAreaForMonitor(
-                this.monitor.index
+        // Get the full workArea here and not workspaceBounds which have gaps
+        const workArea = Main.layoutManager.getWorkAreaForMonitor(
+            this.monitor.index
+        );
+
+        const shift =
+            (workArea.width * newMetaWindows.length) / WINDOW_PER_SCREEN;
+
+        if (!this.animationInProgress) {
+            this.transitionContainer.set_position(
+                workArea.x + (direction > 0 ? 0 : -shift),
+                workArea.y
             );
 
-            const shift =
-                (workArea.width * newMetaWindows.length) / WINDOW_PER_SCREEN;
+            this.overContainer.set_clip(
+                this.monitor.x,
+                this.monitor.y,
+                this.monitor.width,
+                this.monitor.height
+            );
+            global.window_group.add_actor(this.overContainer);
+            this.animationInProgress = true;
+        }
 
-            if (!this.animationInProgress) {
-                this.transitionContainer.set_position(
-                    workArea.x + (direction > 0 ? 0 : -shift),
-                    workArea.y
-                );
-
-                this.overContainer.set_clip(
-                    this.monitor.x,
-                    this.monitor.y,
-                    this.monitor.width,
-                    this.monitor.height
-                );
-                global.window_group.add_actor(this.overContainer);
-                this.animationInProgress = true;
+        Tweener.addTween(this.transitionContainer, {
+            x: workArea.x + (direction > 0 ? -shift : 0),
+            time: WINDOW_SLIDE_TWEEN_TIME,
+            transition: 'easeOutQuad',
+            onComplete: () => {
+                this.animationInProgress = false;
+                this.endTransition();
             }
-
-            Tweener.addTween(this.transitionContainer, {
-                x: workArea.x + (direction > 0 ? -shift : 0),
-                time: WINDOW_SLIDE_TWEEN_TIME,
-                transition: 'easeOutQuad',
-                onComplete: () => {
-                    this.animationInProgress = false;
-                    this.endTransition();
-                }
-            });
         });
     }
 
