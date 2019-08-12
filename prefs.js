c4f349b9fbe9f555105baf7cf8ce001ccb7fa412
Florian Mounier <paradoxxx.zero@gmail.com>
2019-08-12 16:24:43 +0200

    Put back prefs utils in prefs

--- prefs.js
+++ prefs.js
@@ -1,11 +1,54 @@
 const { GObject, Gtk, Gio } = imports.gi;
 
 const Me = imports.misc.extensionUtils.getCurrentExtension();
-const { makePage, makeItemRow, makeItemList } = Me.imports.utils.prefsUi;
 const { getSettings } = Me.imports.utils.settings;
 
 // eslint-disable-next-line no-unused-vars
 function init() {}
+
+const makePage = (title, content) => {
+    const tabWindow = new Gtk.ScrolledWindow({ vexpand: true });
+    tabWindow.add(content);
+    const tabLabel = new Gtk.Label({
+        label: title,
+        halign: Gtk.Align.START,
+        use_markup: false
+    });
+    return [tabWindow, tabLabel];
+};
+
+const makeItemRow = (name, description, item) => {
+    const nameLabel = new Gtk.Label({ xalign: 0 });
+    nameLabel.set_markup(`<span size="medium">${name}</span>`);
+
+    const descriptionLabel = new Gtk.Label({ xalign: 0 });
+    descriptionLabel.set_markup(`<span size="small">${description}</span>`);
+
+    const labelsVBox = new Gtk.VBox();
+    labelsVBox.pack_start(nameLabel, false, false, 0);
+    labelsVBox.pack_start(descriptionLabel, false, false, 0);
+
+    const rowHBox = new Gtk.HBox();
+    rowHBox.pack_start(labelsVBox, true, true, 10);
+    rowHBox.pack_start(item, false, false, 0);
+
+    const listRow = new Gtk.ListBoxRow();
+    listRow.add(rowHBox);
+
+    return listRow;
+};
+
+const makeItemList = rows => {
+    const listBox = new Gtk.ListBox({
+        valign: Gtk.Align.START
+    });
+    const listWrapper = new Gtk.VBox({
+        border_width: 10
+    });
+    rows.forEach(row => listBox.add(row));
+    listWrapper.add(listBox);
+    return listWrapper;
+};
 
 const hotKeysLabels = {
     'previous-window': 'Focus the previous window',
