d7ca487c50f09449bf35741dd06cc9b07190d2a0
PapyElGringo <adrien.peslerbe@bemyeye.com>
2019-09-06 09:27:44 +0200

    clean commented code

--- utils/windows.js
+++ utils/windows.js
@@ -13,7 +13,6 @@ var updateTitleBarVisibility = function(window) {
 };
 
 var setTitleBarVisibility = function(window, visible) {
-    //if (!_handleWindow(window)) return;
     let windowXID = getWindowXID(window);
     if (!windowXID) return;
 
@@ -31,65 +30,7 @@ var setTitleBarVisibility = function(window, visible) {
 
     window.titleBarVisible = visible;
 };
-/* 
-var _setHintValue = function(win, hint, value) {
-    let winId = getWindowXID(win);
-    if (!winId) return;
 
-    Util.spawn(['xprop', '-id', winId, '-f', hint, '32c', '-set', hint, value]);
-};
-
-var _getHintValue = function(win, hint) {
-    let winId = getWindowXID(win);
-    if (!winId) return;
-
-    let result = GLib.spawn_command_line_sync(`xprop -id ${winId} ${hint}`);
-    let string = ByteArray.toString(result[1]);
-    if (!string.match(/=/)) return;
-
-    string = string
-        .split('=')[1]
-        .trim()
-        .split(',')
-        .map(part => {
-            part = part.trim();
-            return part.match(/\dx/) ? part : `0x${part}`;
-        });
-
-    return string;
-};
-
-var _getMotifHints = function(win) {
-    if (!win._GTKTitleBarOriginalState) {
-        let state = _getHintValue(win, '_GTKTitleBar_ORIGINAL_STATE');
-
-        if (!state) {
-            state = _getHintValue(win, '_MOTIF_WM_HINTS');
-            state = state || ['0x2', '0x0', '0x1', '0x0', '0x0'];
-
-            //_setHintValue(win, '_GTKTitleBar_ORIGINAL_STATE', state.join(', '));
-        }
-
-        win._GTKTitleBarOriginalState = state;
-    }
-
-    return win._GTKTitleBarOriginalState;
-};
-
-var _handleWindow = function(win) {
-    let handleWin = false;
-    let meta = Meta.WindowType;
-    let types = [meta.NORMAL];
-    if (!types.includes(win.window_type) || win.find_root_ancestor() !== win)
-        return;
-
-    let state = _getMotifHints(win);
-    handleWin = !win.is_client_decorated();
-    handleWin = handleWin && (state[2] != '0x2' && state[2] != '0x0');
-
-    return handleWin;
-};
- */
 var getWindowXID = function(win) {
     let desc = win.get_description() || '';
     let match = desc.match(/0x[0-9a-f]+/) || [null];
