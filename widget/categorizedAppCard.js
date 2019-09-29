c1a4464d9923e3019e8ff6ad40377448cdb97a9e
Paul Sammut <paul@pop-os.localdomain>
2019-09-29 20:22:26 +1000

    Don't add app to categorizedAppCard if appSys.lookup_app is null. e.g if the app has been uninstalled.

--- widget/categorizedAppCard.js
+++ widget/categorizedAppCard.js
@@ -50,11 +50,13 @@ var CategorizedAppCard = GObject.registerClass(
         _loadApps(apps) {
             let icons = [];
             for (let i = 0; i < Math.min(apps.length, 12); i++) {
-                let app = this.appSys.lookup_app(apps[i]);
-                let appIcon = new AppIcon(app, {
-                    isDraggable: true
-                });
-                icons.push(appIcon);
+                const app = this.appSys.lookup_app(apps[i]);
+                if (app) {
+                    const appIcon = new AppIcon(app, {
+                        isDraggable: true
+                    });
+                    icons.push(appIcon);
+                }
             }
             this._grid.loadIcons(icons);
         }
