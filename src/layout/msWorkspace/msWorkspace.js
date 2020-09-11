6a15ee6fda36e3d6a8497400d4feea0cc45d8111
LFOM <4441874-lfomartins@users.noreply.gitlab.com>
2020-09-11 17:54:58 -0300

    Fixed duplicated code due to failed patch.

--- src/layout/msWorkspace/msWorkspace.js
+++ src/layout/msWorkspace/msWorkspace.js
@@ -248,10 +248,6 @@ var MsWorkspace = class MsWorkspace {
         this.focusTileable(this.tileableList[this.tileableList.length - 1]);
     }
 
-    focusAppLauncher() {
-        this.focusTileable(this.tileableList[this.tileableList.length - 1]);
-    }
-
     focusTileable(tileable, forced) {
         if (tileable === this.tileableFocused && !forced) {
             return;
