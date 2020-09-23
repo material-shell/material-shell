af6d08687b0fd619573f862572f3f2bb775b7cf6
PapyElGringo <adrienpeslerbe@gmail.com>
2020-09-23 10:59:54 +0200

    fix notification manager for 3.34

--- src/widget/msApplicationLauncher.js
+++ src/widget/msApplicationLauncher.js
@@ -8,7 +8,6 @@ const { SetAllocation, Allocate } = Me.imports.src.utils.compatibility;
 const { ShellVersionMatch } = Me.imports.src.utils.compatibility;
 const { AppsManager } = Me.imports.src.manager.appsManager;
 const { MatButton } = Me.imports.src.widget.material.button;
-const { log } = Me.imports.src.utils.debug;
 
 /* exported MsApplicationLauncher */
 
