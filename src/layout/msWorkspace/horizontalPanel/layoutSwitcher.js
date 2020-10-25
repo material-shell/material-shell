8f34f28a440858cfeb881723b90768f0ef54b4a6
PapyElGringo <adrienpeslerbe@gmail.com>
2020-10-25 11:40:08 +0100

    fix save layout

--- src/layout/msWorkspace/horizontalPanel/layoutSwitcher.js
+++ src/layout/msWorkspace/horizontalPanel/layoutSwitcher.js
@@ -128,17 +128,19 @@ var LayoutSwitcher = GObject.registerClass(
         }
 
         removeLayout(layoutKey) {
+            Me.logFocus('removeLayout');
             if (this.msWorkspace.state.layoutStateList.length === 1)
                 return false;
             if (
-                !this.msWorkspace.state.layoutStateList.find(
+                this.msWorkspace.state.layoutStateList.find(
                     (layoutState) => layoutState.key === layoutKey
-                ) != null
+                ) === null
             )
                 return true;
             const index = this.msWorkspace.state.layoutStateList.findIndex(
                 (layoutState) => layoutState.key === layoutKey
             );
+            Me.logFocus('index to remove', index);
             this.msWorkspace.state.layoutStateList.splice(index, 1);
             Me.stateManager.stateChanged();
 
