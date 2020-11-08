c7e16645e472e0e96dc5bb134e40cc88f40ed79f
NastuzziSamy <samy@nastuzzi.fr>
2020-11-08 20:46:10 +0100

    Ratio layout use portions

--- src/layout/msWorkspace/tilingLayouts/custom/ratio.js
+++ src/layout/msWorkspace/tilingLayouts/custom/ratio.js
@@ -10,67 +10,22 @@ const {
 /* exported RatioLayout */
 var RatioLayout = GObject.registerClass(
     class RatioLayout extends BaseTilingLayout {
-        tileTileable(tileable, box, index, siblingLength) {
-            // Define tileable.x tileable.y tileable.width tileable.height
-            let { x, y, width, height } = this.tile(
-                box,
-                index,
-                index === siblingLength - 1
-            );
-            ({ x, y, width, height } = this.applyGaps(x, y, width, height));
-            tileable.x = x;
-            tileable.y = y;
-            tileable.width = width;
-            tileable.height = height;
-        }
-
-        tile(box, index, last) {
-            const ratio = Me.layoutManager.ratio;
-            let areaBox = {
-                x: box.x1,
-                y: box.y1,
-                width: box.get_width(),
-                height: box.get_height(),
+        updateMainPortionLength(length) {
+            const pushInPortion = (portion) => {
+                if (portion.children.length === 2) {
+                    pushInPortion(portion.children[1]);
+                } else {
+                    portion.push();
+                }
             };
-            return new Array(index + 1).fill().reduce((area, _, i) => {
-                // If it's the last iteration
-                if (index === i) {
-                    // If it's the last window, it takes all the remaining space
-                    if (last) {
-                        return area;
-                    }
-                    // Otherwise it's a simple split from available space
-                    if (area.width > area.height) {
-                        return {
-                            x: area.x,
-                            y: area.y,
-                            width: area.width * ratio,
-                            height: area.height,
-                        };
-                    }
-                    return {
-                        x: area.x,
-                        y: area.y,
-                        width: area.width,
-                        height: area.height * ratio,
-                    };
-                }
-                // If it's a normal iteration compute remaining space atfer split
-                if (area.width > area.height) {
-                    return {
-                        x: area.x + area.width * ratio,
-                        y: area.y,
-                        width: area.width * (1 - ratio),
-                        height: area.height,
-                    };
-                }
-                return {
-                    x: area.x,
-                    y: area.y + area.height * ratio,
-                    width: area.width,
-                    height: area.height * (1 - ratio),
-                };
-            }, areaBox);
+
+            while (this.mainPortion.portionLength > length) {
+                this.mainPortion.pop();
+            }
+
+            while (length > 1 && this.mainPortion.portionLength < length) {
+                pushInPortion(this.mainPortion);
+            }
         }
     }
 );
