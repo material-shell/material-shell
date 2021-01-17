c26dcafc456d70fbc8d7f127274849842a09555a
Aron Granberg <aron.granberg@gmail.com>
2021-01-18 00:58:53 +0100

    Throw exception early if settings schema cannot be found

--- src/utils/settings.ts
+++ src/utils/settings.ts
@@ -5,11 +5,18 @@ import * as Gio from 'gio';
 const Me = imports.misc.extensionUtils.getCurrentExtension();
 
 /* exported getSettings */
-export const getSettings = (key) =>
-    new Gio.Settings({
-        settings_schema: Gio.SettingsSchemaSource.new_from_directory(
-            Me.dir.get_child('schemas').get_path(),
-            Gio.SettingsSchemaSource.get_default(),
-            false
-        ).lookup(Me.metadata[key], true),
-    });
+export const getSettings = (key: string) => {
+    let schema = Gio.SettingsSchemaSource.new_from_directory(
+        Me.dir.get_child('schemas').get_path(),
+        Gio.SettingsSchemaSource.get_default(),
+        false
+    ).lookup(Me.metadata[key], true);
+
+    if (schema !== null) {
+        return new Gio.Settings({
+            settings_schema: schema,
+        });
+    } else {
+        throw Error("Cannot find schema");
+    }
+}
