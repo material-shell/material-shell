64fd159515556d0ddae210efa4d3eefa3e874297
Aron Granberg <aron.granberg@gmail.com>
2021-01-19 22:18:43 +0100

    Improve asserts

--- src/utils/assert.ts
+++ src/utils/assert.ts
@@ -1,7 +1,14 @@
 import * as GLib from 'GLib';
 
-export const logAssert = (shouldBeTrue: boolean, message: string): boolean => {
-    if (shouldBeTrue) {
+export function assert (shouldBeTrue: boolean, message: string): asserts shouldBeTrue {
+    if (!shouldBeTrue) {
+        throw Error("Assertion failed: " + message)
+    }
+}
+
+
+export function logAssert (shouldBeTrue: boolean, message: string): boolean {
+    if (!shouldBeTrue) {
         GLib.log_structured("Material Shell", GLib.LogLevelFlags.FLAG_FATAL, {
             MESSAGE: "Assertion failed: " + message,
             STACKTRACE: new Error().stack,
