0d2d8e238bb224d09aa4727a8bff1d12e42fc180
Aron Granberg <aron.granberg@gmail.com>
2021-01-23 20:39:10 +0100

    Translate NumberPicker to ts

--- src/widget/material/numberPicker.ts
+++ src/widget/material/numberPicker.ts
@@ -8,77 +8,86 @@ import * as Gio from 'Gio';
 const Me = imports.misc.extensionUtils.getCurrentExtension();
 import { SetAllocation, Allocate } from 'src/utils/compatibility';
 import { RippleBackground } from 'src/widget/material/rippleBackground';
+import { registerGObjectClass } from 'src/utils/gjs';
 
-export const MatNumberPicker = GObject.registerClass(
-    {
+@registerGObjectClass
+export class MatNumberPicker extends St.BoxLayout {
+    static metaInfo: GObject.MetaInfo = {
         GTypeName: 'MatNumberPicker',
         Signals: {
             changed: {
                 param_types: [GObject.TYPE_INT],
             },
         },
-    },
-    class MatNumberPicker extends St.BoxLayout {
-        _init(value, params) {
-            super._init({
-                y_align: Clutter.ActorAlign.CENTER,
-            });
-            this.value = value;
-            this.params = Object.assign(
-                {
-                    step: 1,
-                    min: 0,
-                    max: 999999,
-                },
-                params
-            );
-            this.minIcon = new St.Icon({
-                gicon: Gio.icon_new_for_string(
-                    `${Me.path}/assets/icons/minus-symbolic.svg`
-                ),
-            });
-            this.minButton = new St.Button({
-                child: this.minIcon,
-            });
-            this.minButton.connect('clicked', () => {
-                this.decrement();
-            });
-            this.plusIcon = new St.Icon({
-                gicon: Gio.icon_new_for_string(
-                    `${Me.path}/assets/icons/plus-symbolic.svg`
-                ),
-            });
-            this.plusButton = new St.Button({
-                child: this.plusIcon,
-            });
-            this.plusButton.connect('clicked', () => {
-                this.increment();
-            });
-            this.valueLabel = new St.Label({
-                text: this.value.toString(),
-                y_align: Clutter.ActorAlign.CENTER,
-            });
-            this.add_child(this.minButton);
-            this.add_child(this.valueLabel);
-            this.add_child(this.plusButton);
-        }
-        decrement() {
-            this.value = Math.max(
-                this.params.min,
-                this.value - this.params.step
-            );
-            this.updateValue();
-        }
-        increment() {
-            this.value = Math.min(
-                this.params.max,
-                this.value + this.params.step
-            );
-            this.updateValue();
-        }
-        updateValue() {
-            this.valueLabel.text = this.value.toString();
-            this.emit('changed', this.value);
-        }
     }
-);
+
+    value: number;
+    params: any;
+    minIcon: St.Icon;
+    minButton: St.Button<St.Icon>;
+    plusIcon: St.Icon;
+    plusButton: St.Button<St.Icon>;
+    valueLabel: St.Label;
+
+    constructor(value: number, params) {
+        super({
+            y_align: Clutter.ActorAlign.CENTER,
+        });
+        this.value = value;
+        this.params = Object.assign(
+            {
+                step: 1,
+                min: 0,
+                max: 999999,
+            },
+            params
+        );
+        this.minIcon = new St.Icon({
+            gicon: Gio.icon_new_for_string(
+                `${Me.path}/assets/icons/minus-symbolic.svg`
+            ),
+        });
+        this.minButton = new St.Button({
+            child: this.minIcon,
+        });
+        this.minButton.connect('clicked', () => {
+            this.decrement();
+        });
+        this.plusIcon = new St.Icon({
+            gicon: Gio.icon_new_for_string(
+                `${Me.path}/assets/icons/plus-symbolic.svg`
+            ),
+        });
+        this.plusButton = new St.Button({
+            child: this.plusIcon,
+        });
+        this.plusButton.connect('clicked', () => {
+            this.increment();
+        });
+        this.valueLabel = new St.Label({
+            text: this.value.toString(),
+            y_align: Clutter.ActorAlign.CENTER,
+        });
+        this.add_child(this.minButton);
+        this.add_child(this.valueLabel);
+        this.add_child(this.plusButton);
+    }
+    decrement() {
+        this.value = Math.max(
+            this.params.min,
+            this.value - this.params.step
+        );
+        this.updateValue();
+    }
+    increment() {
+        this.value = Math.min(
+            this.params.max,
+            this.value + this.params.step
+        );
+        this.updateValue();
+    }
+    updateValue() {
+        this.valueLabel.text = this.value.toString();
+        this.emit('changed', this.value);
+    }
+}
