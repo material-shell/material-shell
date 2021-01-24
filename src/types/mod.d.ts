9efc2bbd24ff51489c7e019cd11a9d8ddf4536e4
Aron Granberg <aron.granberg@gmail.com>
2021-01-24 14:32:57 +0100

    Clean up type definitions

--- src/types/mod.d.ts
+++ src/types/mod.d.ts
@@ -130,57 +130,6 @@ interface DialogButtonAction {
 declare type ProcessResult = [boolean, any, any, number];
 declare type SignalID = number;
 
-// declare interface GLib {
-//     PRIORITY_DEFAULT: number;
-//     PRIORITY_LOW: number;
-
-//     file_test(filename: String, test: GLib.FileTest): boolean;
-//     find_program_in_path(prog: string): string | null;
-//     get_current_dir(): string;
-//     get_monotonic_time(): number;
-
-//     idle_add(priority: any, callback: () => boolean): number;
-
-//     signal_handler_block(object: GObject.Object, signal: SignalID): void;
-//     signal_handler_unblock(object: GObject.Object, signal: SignalID): void;
-
-//     source_remove(id: SignalID): void;
-//     spawn_command_line_sync(cmd: string): ProcessResult;
-//     spawn_command_line_async(cmd: string): boolean;
-
-//     timeout_add(priority: number, ms: number, callback: () => Boolean): number;
-// }
-
-// declare namespace GLib {
-//     class FileTest {
-//         static IS_REGULAR: number;
-//         static IS_SYMLINK: number;
-//         static IS_DIR: number;
-//         static IS_EXECUTABLE: number;
-//         static EXISTS: number;
-//     }
-// }
-
-// declare interface Gio {
-//     // TOOD: Shouldn't this be a static method on File?
-//     file_new_for_path(path: String): Gio.File;
-// }
-
-// declare namespace Gio {
-//     interface File {
-//         load_contents_async(cancellable: Gio.Cancellable|null, callback: Gio.AsyncReadyCallback): void;
-//     }
-
-//     interface Cancellable extends GObject.Object {
-//     }
-
-//     interface AsyncResult {
-//         get_user_data(): any;
-//     }
-
-//     type AsyncReadyCallback = (source_object: GObject.Object, res: Gio.AsyncResult)=>void
-// }
-
 declare module "GObject" {
     // Why these are not in the documentation or auto-generated typedefs, I do not know.
     export const TYPE_INT: GObject.GType<number>;
@@ -252,18 +201,6 @@ declare namespace Gtk {
 }
 
 declare module "Clutter" {
-    // enum ActorAlign {
-    //     FILL = 0,
-    //     START = 1,
-    //     CENTER = 3,
-    //     END = 3
-    // }
-
-    // enum AnimationMode {
-    //     EASE_IN_QUAD = 2,
-    //     EASE_OUT_QUAD = 3,
-    // }
-
     type AnimatableActorFields = "fixed_x" | "fixed_y" | "height" | "margin_bottom" | "margin_left" | "margin_right" | "margin_top" | "min_height" | "min_width" | "natural_height" | "natural_width" | "opacity" | "pivot_point_z" | "rotation_angle_x" | "rotation_angle_y" | "rotation_angle_z" | "scale_x" | "scale_y" | "scale_z" | "translation_x" | "translation_y" | "translation_z" | "width" | "x" | "y" | "z_position";
 
     interface EasingParams {
@@ -283,37 +220,10 @@ declare module "Clutter" {
     }
 
     interface Actor extends Rectangular, GObject.Object {
-        // add(child: Actor): void;
-        // add_child(child: Actor): void;
-        // destroy(): void;
-        // destroy_all_children(): void;
-
         // Some extensions added by gnome-shell in gnome-shell/js/ui/environment.js->init
         ease(params: EasingParamsWithProperties): void;
         ease_property(propName: AnimatableActorFields, target: number, params: EasingParams): void;
         ease_property(propName: "@effects.dimmer.brightness", target: Clutter.Color, params: EasingParams): void;
-
-        // hide(): void;
-        // get_child_at_index(nth: number): Actor | null;
-        // get_n_children(): number;
-        // get_parent(): Actor | null;
-        // get_stage(): Actor | null;
-        // get_transition(param: string): any | null;
-        // grab_key_focus(): void;
-        // is_visible(): boolean;
-        // queue_redraw(): void;
-        // remove_all_children(): void;
-        // remove_all_transitions(): void;
-        // remove_child(child: Actor): void;
-        // set_child_above_sibling(child: Actor, sibling: Actor | null): void;
-        // set_child_below_sibling(child: Actor, sibling: Actor | null): void;
-        // set_easing_duration(msecs: number | null): void;
-        // set_opacity(value: number): void;
-        // set_size(width: number, height: number): void;
-        // set_y_align(align: ActorAlign): void;
-        // set_position(x: number, y: number): void;
-        // set_size(width: number, height: number): void;
-        // show(): void;
     }
 
     // The Clutter.d.ts file doesn't have proper 'extends' information
@@ -323,12 +233,6 @@ declare module "Clutter" {
     interface ActorBox {
         new(x: number, y: number, width: number, height: number): ActorBox;
     }
-
-
-    // interface Text {
-    //     get_text(): Readonly<string>;
-    //     set_text(text: string | null): void;
-    // }
 }
 
 declare namespace Shell {
@@ -349,34 +253,3 @@ declare namespace Shell {
         setInitialKeyFocus(actor: Clutter.Actor): void;
     }
 }
-
-
-
-// export declare namespace St {
-//     interface Adjustment{}
-//     interface Bin{}
-//     interface BorderImage{}
-//     interface BoxLayout{}
-//     interface Button{}
-//     interface Clipboard{}
-//     interface DrawingArea{}
-//     interface Entry{}
-//     interface FocusManager{}
-//     interface GenericAccessible{}
-//     interface Icon{}
-//     interface ImageContent{}
-//     interface Label{}
-//     interface PasswordEntry{}
-//     interface ScrollBar{}
-//     interface ScrollView{}
-//     interface ScrollViewFade{}
-//     interface Scrollable{}
-//     interface Settings{}
-//     interface TextureCache{}
-//     interface Theme{}
-//     interface ThemeContext{}
-//     interface ThemeNode{}
-//     interface Viewport{}
-//     interface Widget{}
-//     interface WidgetAccessible{}
-// }
