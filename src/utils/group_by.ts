91ee9e113d05094a2978c836ec67686fe2122acc
Aron Granberg <aron.granberg@gmail.com>
2022-05-30 16:41:18 +0200

    Add groupBy utility function.

---
+++ src/utils/group_by.ts
@@ -0,0 +1,14 @@
+/** Groups the given items into a map by their keys */
+export function groupBy<T,K>(items: T[], key: (item: T)=>K): Map<K, T[]> {
+    let map = new Map<K,T[]>();
+    for (const item of items) {
+        const k = key(item);
+        let ls = map.get(k);
+        if (ls === undefined) {
+            ls = [];
+            map.set(k, ls);
+        }
+        ls.push(item);
+    }
+    return map;
+}
