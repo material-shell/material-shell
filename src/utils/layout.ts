fa3e8d23439d7214ce5dd50c203b44236b774fdf
Aron Granberg <aron.granberg@gmail.com>
2022-06-26 14:41:43 +0200

    Fix unresponsive windows breaking everything.

---
+++ src/utils/layout.ts
@@ -0,0 +1,13 @@
+import * as Clutter from 'clutter';
+
+export function centerInBox(
+    box: Clutter.ActorBox,
+    width: number,
+    height: number
+) {
+    const paddingX = (box.x2 - box.x1 - width) / 2;
+    const paddingY = (box.y2 - box.y1 - height) / 2;
+    const x = box.x1 + paddingX;
+    const y = box.y1 + paddingY;
+    return Clutter.ActorBox.new(x, y, x + width, y + height);
+}
